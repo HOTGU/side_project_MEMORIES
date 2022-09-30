ec81daeb761138398c2727a32207ce7f5c3b2ae8
hangu <gksrn42@gmail.com>
2022-09-30 11:39:27 +0900

    env plus

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
 const handleListen = () => {
-    console.log("서버가 실행중이야");
+    console.log(`서버가 ${process.env.PORT}에서 실행중이야`);
 };
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, handleListen);
+app.listen(process.env.PORT, handleListen);
